7f86db53be4fa2d9c355f5455eccd26f8685ca08
HuijingJia <134741223+HuijingJia@users.noreply.github.com>
2023-09-01 14:05:36 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import generateName from "sillyname";
+import superheroes from "superheroes";
+
+var name = generateName();
+console.log(`My name is ${name}.`);
+
+import hero from "superheroes";
+const heroName = superheroes.random();
+
+console.log(`My hero is ${heroName}.`);
